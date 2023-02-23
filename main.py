66aab4f62d6e3f21f0c4bc399f8de2565bb709b6
mallie2000 <muzammilallie230@gmail.com>
2023-02-23 00:30:11 -0800

    Finished backwards selection

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ def retrieve_num_of_features():
 
 def retrieve_search_algo_choice():
     search_algo_number = 0
-    algorithms = ["Forward Selection","Backwards Elimination", "Muzammil's Special Search Algo"]
-    while(search_algo_number not in range(1,4)):
+    list_of_algorithms = ["Forward Selection","Backwards Elimination", "Muzammil's Special Search Algo"]
+    while(search_algo_number not in range(1,len(list_of_algorithms)+1)):
         print("Type the number of the algorithm you wish to run")
-        for index, algo in enumerate(algorithms):
+        for index, algo in enumerate(list_of_algorithms):
             print(f"{index+1} {algo}")
         search_algo_number = int(input("Enter your choice here: "))
         print()
     return search_algo_number
 
 def main():
-    number_of_features = features()
-    search_algo = select_algo()
     s = SearchAlgo()
+    number_of_features = retrieve_num_of_features()
+    search_algo = retrieve_search_algo_choice()
     if(search_algo == 1):
         s.forwardSelection(number_of_features)
-    
+    if(search_algo == 2):
+        s.backwards_elimination(number_of_features)
 
 if __name__ == "__main__":
     main()
--- node.py
+++ node.py
@@ -1,26 +1,26 @@
 import random as r
+from copy import deepcopy
+def remove_values_from_list(original_list,values_to_remove):
+    for value in values_to_remove:
+        if(value in original_list):
+            original_list.remove(value)
+    return original_list
 class Node:
     def __init__(self,feature_set = None):
-        if feature_set != None:
-            self.feature_set = feature_set
-        else:
-            self.feature_set = []
+        self.feature_set = []
         self.score = 0
         self.evaluation_function()
+        if(feature_set is not None):
+            self.feature_set = feature_set
 
     def evaluation_function(self):
         self.score = round(r.uniform(0,100.00),2)
 
 class SearchAlgo:
     def __init__(self):
-        self.max_accuracy = 0
-        self.best_feature_set = []
+        self.highest_accuracy = 0
+        self.feature_set_with_highest_accuracy = []
 
-    def removeValuesFromFeatureList(self,original_list,values_to_remove):
-        for value in values_to_remove:
-            if(value in original_list):
-                original_list.remove(value)
-        return original_list
 
     def exploreBestFeatures(self,list_of_features,initialFeatures = None):
         nodes_to_explore = []
@@ -35,22 +35,51 @@ class SearchAlgo:
         print()
         best_feature = nodes_to_explore.pop()
         print(f"Feature set {best_feature.feature_set} was best, accuracy is {best_feature.score}%")
-        if(best_feature.score > self.max_accuracy):
-            self.max_accuracy = best_feature.score
-            self.best_feature_set = best_feature.feature_set
+        if(best_feature.score > self.highest_accuracy):
+            self.highest_accuracy = best_feature.score
+            self.feature_set_with_highest_accuracy = best_feature.feature_set
         return best_feature
 
     def forwardSelection(self,number_of_features):
         startingNode = Node()
         print(f"Using no features and 'random' evaluations, I get an accuracy of {startingNode.score}%\n")
-        self.max_accuracy = startingNode.score
+        self.highest_accuracy = startingNode.score
         print("Beginning search\n")
         feature_list  = [x for x in range(1,number_of_features+1)]
         best_node_feature_set = None
         while(len(feature_list) != 0):
             best_node_feature_set = self.exploreBestFeatures(feature_list,best_node_feature_set).feature_set
-            feature_list = self.removeValuesFromFeatureList(feature_list,best_node_feature_set)
-        print(f"Finished Search!! The best feature subset is {self.best_feature_set}, which has an accuracy of {self.max_accuracy}%")
+            feature_list = self.remove_values_from_list(feature_list,best_node_feature_set)
+        print(f"\nFinished Search!! The best feature subset is {self.feature_set_with_highest_accuracy}, which has an accuracy of {self.highest_accuracy}%")
+    
+    def backwards_elimination(self,number_of_features):
+        starting_list_of_features = [x for x in range(1,number_of_features+1)]
+        starting_node = Node(starting_list_of_features)
+        print(f"Using all features and 'random' evaluations, I get an accuracy of {starting_node.score}%\n")
+        self.highest_accuracy = starting_node.score
+        self.feature_set_with_highest_accuracy = starting_list_of_features
+        print("Beginning search\n")
+
+        while(len(starting_list_of_features) != 0):
+            list_of_possible_nodes = []
+            for feature in starting_list_of_features:
+                list_of_features_minus_selected = deepcopy(starting_list_of_features)
+                list_of_features_minus_selected.remove(feature)
+                possible_node = Node(list_of_features_minus_selected)
+                list_of_possible_nodes.append(possible_node)
+                print(f"Using feature(s) {possible_node.feature_set} accuracy is {possible_node.score}%")
+
+            list_of_possible_nodes.sort(key = lambda x: x.score)
+            best_possible_node = list_of_possible_nodes.pop()
+
+            if(best_possible_node.score > self.highest_accuracy):
+                self.highest_accuracy = best_possible_node.score
+                self.feature_set_with_highest_accuracy  = best_possible_node.feature_set
+            print(f"Feature set {best_possible_node.feature_set} was best, accuracy is {best_possible_node.score}%\n")
+            starting_list_of_features = best_possible_node.feature_set
+        print(f"\nFinished Search!! The best feature subset is {self.feature_set_with_highest_accuracy}, which has an accuracy of {self.highest_accuracy}%")
+        
+
 
 
 
